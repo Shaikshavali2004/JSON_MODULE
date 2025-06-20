079dbb774266a544f29919f09fa82f79912806b3
Shaik Shaikshavali <shaikshavali1019@gmail.com>
2025-06-20 10:57:24 +0530

    Update readrecepie.py

--- readrecepie.py
+++ readrecepie.py
@@ -23,30 +23,3 @@ print("New File Created: recepie_items.json")
 fp.close()
 
 
-# import requests,json
-
-# api_url='https://dummyjson.com/products'
-# data=requests.get(api_url)
-# product_data=data.json()
-# print(type(product_data))
-# products=product_data['products']
-# print(type(products))
-
-# #Transform data
-
-# beauty_products=[]
-# for product in products:
-#     if product['category']=='beauty':
-#         beauty_products.append({"id":product['id'],"name":product['title'],"type": product['category'],"price":product['price']})
-
-# print(len(beauty_products))
-
-
-# #Load into json file
-
-# fp=open('beauty_products.json','w')
-# json.dump(beauty_products,fp,indent=2)
-
-# print("New File Created: beauty_products.json")
-
-# fp.close()
